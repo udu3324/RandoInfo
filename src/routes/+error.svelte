18cafb6c2aaa9bfdd3635a7700dbc31a5a5a4ea8
udu3324 <builder7177@gmail.com>
2025-01-19 23:46:24 -0600

    add weather get endpoint

---
+++ src/routes/+error.svelte
@@ -0,0 +1,1 @@
+no endpoint exists here!
--- src/routes/weather/+server.js
+++ src/routes/weather/+server.js
@@ -19,11 +19,72 @@ export async function GET() {
         }
     }
 
+    if (forecast === undefined) {
+        return json({
+            sucess: false
+        })
+    }
+
+    //ty https://www.reddit.com/r/EnglishLearning/comments/7o8rdm/the_definitive_scale_of_english_adjectives_to/
+    const tempC = forecast.current.temp_c
+    let tempDescription = ""
+    let emojiDescription = ""
+
+    if (tempC <= -9) {
+        tempDescription = "freezing"
+        emojiDescription = "🧊"
+    } else if (tempC <= 0) {
+        tempDescription = "cold"
+        emojiDescription = "🥶"
+    } else if (tempC <= 4) {
+        tempDescription = "chilly"
+        emojiDescription = "🤧"
+    } else if (tempC <= 8) {
+        tempDescription = "brisk"
+        emojiDescription = "🧥"
+    } else if (tempC <= 13) {
+        tempDescription = "cool"
+        emojiDescription = "😶‍🌫️"
+    } else if (tempC <= 18) {
+        tempDescription = "crisp"
+        emojiDescription = "🍃"
+    } else if (tempC <= 25) {
+        tempDescription = "gorgeous"
+        emojiDescription = "😎"
+    } else if (tempC <= 29) {
+        tempDescription = "warm"
+        emojiDescription = "☀️"
+    } else if (tempC <= 33) {
+        tempDescription = "hot"
+        emojiDescription = "🥵"
+    } else {
+        tempDescription = "boiling"
+        emojiDescription = "🔥"
+    }
+
+    const forecastDescription = forecast.current.condition.text.toLowerCase()
+
+    let appendEnd = " weather"
+    if (forecastDescription.includes(" ") && !forecastDescription.match(/cloudy$/g)) {
+        appendEnd = ""
+    }
+
     return json({
-        text: `${forecast.location.name} in ${forecast.location.country}`,
-        coordinates: `${coordinates[0]}, ${coordinates[1]}`,
-        forecast: forecast,
-        sucess: forecast !== undefined
+        continent: forecast.location.tz_id,
+        local_time: forecast.location.localtime,
+        description: `It is currently ${tempDescription} in ${forecast.location.name} (${forecast.location.country}) with ${forecastDescription}${appendEnd}.`,
+        icon: emojiDescription,
+        temperature: {
+            fahrenheit: forecast.current.temp_f,
+            celcius: forecast.current.temp_c,
+        },
+        coordinates: {
+            latitude: coordinates[0],
+            longitude: coordinates[1]
+        },
+        google_maps: `https://www.google.com/maps?q=${coordinates[0]},${coordinates[1]}`,
+        open_street_maps: `https://www.openstreetmap.org/?mlat=${coordinates[0]}&mlon=${coordinates[1]}`,
+        sucess: true
     })
 }
 
