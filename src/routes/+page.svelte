33fca1f8d9837f98ecce9e2b73c211501c17371c
udu3324 <builder7177@gmail.com>
2025-01-20 02:49:03 -0600

    add swagger

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,17 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<script>
+    import { onMount } from "svelte";
+    import * as swaggerJson from './swagger.json';
+    import SwaggerUI from 'swagger-ui';
+    import 'swagger-ui/dist/swagger-ui.css';
+
+    onMount(async () => {
+        SwaggerUI({
+            spec: swaggerJson,
+            dom_id: '#swagger-ui-container'
+        })
+    })
+</script>
+
+<div id="swagger-ui-container">
+    
+</div>
