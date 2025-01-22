1a08948389094bb464df5277cd4d9ec2855f6e76
udu3324 <builder7177@gmail.com>
2025-01-21 21:17:09 -0600

    add handling of user input

--- src/routes/api/v1/fact/create/+server.js
+++ src/routes/api/v1/fact/create/+server.js
@@ -1,8 +1,29 @@
 import { supabase } from '$lib/supabase.js';
 
 export async function POST({ request, getClientAddress }) {
-    const fact = await request.json()
+    const fact = await request.text().trim()
+
+    if (!fact) {
+        return new Response('Did not provide fact', { status: 401 })
+    }
+
+    if (fact.length < 1) {
+        return new Response('Did not provide fact', { status: 402 })
+    }
+
+    if (typeof fact !== "string") {
+        return new Response('Fact provided is not a string', { status: 403 })
+    }
+
     console.log("added fact:", fact)
-    //supabase.
-    return new Response(`Fact saved sucessfully! ${fact}`, { status: 200 })
+    const { error } = await supabase.from('facts').insert([{
+        fact: fact,
+        ip: getClientAddress()
+    }])
+
+    if (error) {
+        return new Response("Failed to write to database!", { status: 500 })
+    }
+
+    return new Response(`Fact saved sucessfully! "${fact}"`, { status: 200 })
 }
