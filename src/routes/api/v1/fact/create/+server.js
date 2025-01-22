3e397320c4ed3af8b33b8779324d5c19474d1aba
udu3324 <builder7177@gmail.com>
2025-01-21 22:01:39 -0600

    ip check

--- src/routes/api/v1/fact/create/+server.js
+++ src/routes/api/v1/fact/create/+server.js
@@ -1,15 +1,17 @@
 import { supabase } from '$lib/supabase.js';
 
 export async function POST({ request, getClientAddress }) {
+    //check if a fact is already stored from the same user/ip
+    if (alreadyCreatedFact(getClientAddress())) {
+        return new Response("Already created a fact!", { status: 400 })
+    }
+
     let fact
     try {
         fact = await request.json()
     } catch (error) {
         return new Response('Invalid json', { status: 401 })
     }
-
-    console.log(fact)
-    console.log(fact.fact)
 
     if (!fact.fact) {
         return new Response('Did not provide fact', { status: 402 })
@@ -23,16 +25,33 @@ export async function POST({ request, getClientAddress }) {
         return new Response('Fact provided is not a string', { status: 403 })
     }
 
-    console.log("added fact:", fact)
     const { error } = await supabase.from('facts').insert([{
-        fact: fact,
+        fact: fact.fact,
         ip: getClientAddress()
     }])
 
     if (error) {
         console.log("error", error)
-        return new Response("Failed to write to database!", { status: 500 })
+        return new Response("Failed to write to database!", { status: 501 })
     }
 
-    return new Response(`Fact saved sucessfully! "${fact}"`, { status: 200 })
+    console.log("added fact:", fact)
+    return new Response(`Fact saved sucessfully! "${fact.fact}"`, { status: 200 })
+}
+
+async function alreadyCreatedFact(ip) {
+    const { data, error } = await supabase
+    .from('facts')
+    .select('*')
+    .eq('ip', ip)
+    .single()
+
+    //the only error should be the one where a row is not found
+    if (error) {
+        if (error.code === "PGRST116") {
+            return false
+        }
+    }
+
+    return true
 }
