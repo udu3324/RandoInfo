97fb7ecfd671332b3aafdef3313e5fd0263dc0e0
udu3324 <builder7177@gmail.com>
2025-01-22 19:16:13 -0600

    add reporting to webhook

--- src/routes/api/v1/string/report/[path]/+server.js
+++ src/routes/api/v1/string/report/[path]/+server.js
@@ -1,6 +1,44 @@
 import { SLACKWEBHOOKURL } from "$env/static/private"
+import { supabase } from "$lib/supabase.js";
 
 export async function POST({ params, request }) {
     const { path } = params
     console.log("path is", path)
+
+    if (!path) {
+        return new Response("Did not provide string id", { status: 300 })
+    }
+
+    if (typeof path !== "number") {
+        return new Response("String id provided is invalid", { status: 301 })
+    }
+    
+    //search in supabase for a row with the id
+    const { data, error } = await supabase
+        .from('strings')
+        .select('*')
+        .eq('id', id)
+        .single()
+    
+    if (error && error.code === 'PGRST116') {
+        return new Response("No string stored with provided id", { status: 302 })
+    }
+
+    if (error) {
+        return new Response("Failed to read strings database", { status: 500 })
+    }
+
+    const response = await fetch(SLACKWEBHOOKURL, {
+        method: "POST",
+        body: JSON.stringify({
+            id: data.id,
+            reported_string: data.string
+        }),
+    });
+
+    if (!response.ok) {
+        return new Response(`Failed to create a report for string id ${data.id}`, { status: 501 })  
+    }
+
+    return new Response(`Sucessfuly created a report for string id ${data.id}`, { status: 200 })
 }
